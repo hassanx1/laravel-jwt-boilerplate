604841112aaf7d04876230058f646e565eb08029
Prinsoft  Group <prinsoftgroup@gmail.com>
2021-08-12 21:38:05 +0300

    created apicontroller,  productController and product model

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -55,5 +55,73 @@ class ApiController extends Controller
             'email' => 'required|email',
             'password' => 'required|string|min:6|max:50'
         ]);
+
+        // send failed response if request is not valid
+        if($validator->fails()){
+            return response()->json(['error' => $validator->getMessageBag()], 200);
+        }
+
+        // request is validated
+        // create token
+        try{
+            if(! $token = JWTAuth::attempt($credentials))
+            {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Login credentials are invalid.',
+                ], 400);
+            }
+        }catch(JWTException $e){
+            return $credentials;
+            return response()->json([
+                'success' => false,
+                'message' => 'Could not create token.',
+            ], 500);
+        }
+
+        // Token created, return with success response and jwt token
+        return response()->json([
+            'success' => true,
+            'token' => $token,
+        ]);
+
+    }
+
+    public function logout(Request $request)
+    {
+        // valid credentials
+        $validator = Validator::make($request->only('token'),[
+            'token' => 'required'
+        ]);
+
+        // send failed response if request is not valid
+        if($validator->fails()){
+            return response()->json(['error' => $validator->getMessageBag()], 200);
+        }
+
+        // request is validated, do logout
+        try{
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User has been logged out'
+            ]);
+        }catch(JWTException $exception){
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, user cannot be logged out'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function get_user(Request $request)
+    {
+        $this->validate($request,[
+            'token' => 'required'
+        ]);
+
+        $user = JWTAuth::authenticate($request->token);
+        return response()->json(['user' => $user]);
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = JWTAuth::parseToken()->authenticate();
+
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +25,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return $this->user->products()
+        ->get();
     }
 
     /**
@@ -35,7 +47,34 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         //Validate data
+        $data = $request->only('name', 'sku', 'price', 'quantity');
+        $validator = Validator::make($data, [
+            'name' => 'required|string',
+            'sku' => 'required',
+            'price' => 'required',
+            'quantity' => 'required'
+        ]);
+
+         //Send failed response if request is not valid
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->getMessageBag()], 200);
+        }
+
+         //Request is valid, create new product
+        $product = $this->user->products()->create([
+            'name' => $request->name,
+            'sku' => $request->sku,
+            'price' => $request->price,
+            'quantity' => $request->quantity
+        ]);
+
+        //Product created, return success response
+        return response()->json([
+            'success' => true,
+            'message' => 'Product created successfully',
+            'data' => $product
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -44,9 +83,18 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $product)
+    public function show($id)
     {
-        //
+         $product = $this->user->products()->find($id);
+
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, product not found.'
+            ], 400);
+        }
+
+        return $product;
     }
 
     /**
@@ -69,7 +117,34 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+         //Validate data
+        $data = $request->only('name', 'sku', 'price', 'quantity');
+        $validator = Validator::make($data, [
+            'name' => 'required|string',
+            'sku' => 'required',
+            'price' => 'required',
+            'quantity' => 'required'
+        ]);
+
+        //Send failed response if request is not valid
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->getMessageBag()], 200);
+        }
+
+         //Request is valid, update product
+        $product = $product->update([
+            'name' => $request->name,
+            'sku' => $request->sku,
+            'price' => $request->price,
+            'quantity' => $request->quantity
+        ]);
+
+        //Product updated, return success response
+        return response()->json([
+            'success' => true,
+            'message' => 'Product updated successfully',
+            'data' => $product
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -80,6 +155,11 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+         $product->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product deleted successfully'
+        ], Response::HTTP_OK);
     }
 }
